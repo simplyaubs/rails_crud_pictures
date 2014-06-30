9e705f7c20ec9d3f68219741be08953e6d4c0219
Aubrey Howell <simplyaubs@gmail.com>
2014-06-29 22:09:33 -0600

    User can update a picture from list.

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -15,6 +15,21 @@ class PicturesController < ApplicationController
     end
   end
 
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+    @picture.update_attributes!(pictures_params)
+
+    redirect_to pictures_path
+  end
+
   private
   def pictures_params
     params.require(:picture).permit(:content, :size)
--- spec/features/pictures_spec.rb
+++ spec/features/pictures_spec.rb
@@ -11,4 +11,26 @@ feature 'CRUD pictures' do
     expect(page).to have_content 'Cats'
     expect(page).to have_content 'Medium'
   end
+
+  scenario 'User can update a picture from list' do
+    visit '/'
+    expect(page).to have_content 'Welcome'
+    click_on 'Add a picture'
+    fill_in 'Content', with: 'Cats'
+    fill_in 'Size', with: 'Medium'
+    click_on 'Add picture'
+    expect(page).to have_content 'Cats'
+    expect(page).to have_content 'Medium'
+    click_on 'Cats'
+    expect(page).to have_content 'Cats'
+    expect(page).to have_content 'Medium'
+    click_on 'Edit'
+    fill_in 'Content', with: 'Dogs'
+    fill_in 'Size', with: 'Small'
+    click_on 'Update picture'
+    expect(page).to have_content 'Dogs'
+    expect(page).to have_content 'Small'
+    expect(page).to_not have_content 'Cats'
+    expect(page).to_not have_content 'Medium'
+  end
 end
